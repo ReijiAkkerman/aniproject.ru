999b66b8a060e27099b5eb30e469f236a236822b
ReijiAkkerman <reijiakkerman@gmail.com>
2024-01-25 20:26:39 +0300

    Переделал обработку свойства border чтобы управлять изменениями цвета из одного файла

--- app/view/pages/calendar.php
+++ app/view/pages/calendar.php
@@ -6,14 +6,16 @@
     <body class="calendar">
     <?php include_once __DIR__ . "/components/common/header.php" ?>
         <main class="Calendar">
-            <div class="Day">
+        <?php for($i = 0; $i < 63;$i++) { ?>
+            <div class="Day <?php if($i % 7 == 0) echo "start" ?>">
                 <div class="Header">
-                    
+                    <button>01/01</button>
                 </div>
                 <div class="Body">
 
                 </div>
             </div>
+        <?php } ?>
         </main>
     </body>
 </html>
