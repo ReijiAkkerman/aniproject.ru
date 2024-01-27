5e7642dc8c2a8f2b0577e1418e98cd891b47390c
ReijiAkkerman <reijiakkerman@gmail.com>
2024-01-28 00:07:36 +0300

    Закончил дизайн формы для создания записей

--- app/view/pages/calendar.php
+++ app/view/pages/calendar.php
@@ -189,7 +189,40 @@
                     </div>
                 </div>
                 <div class="Adds">
-                    
+                    <div>
+                        <p class="bold">Категории</p>
+                        <div class="list">
+                            <p>Категория</p>
+                            <button>
+                                <svg viewBox="0 0 1024 1024">
+                                <path d="M831.872 340.864 512 652.672 192.128 340.864a30.592 30.592 0 0 0-42.752 0 29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728 30.592 30.592 0 0 0-42.752 0z"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <div class="recent">
+                            <div>
+                                <input type="checkbox" name="user<?= "" ?>" value="" id="cath">
+                                <label for="cath">Cathegory</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="bold">Пользователи</p>
+                        <div class="list">
+                            <p>Пользователь</p>
+                            <button>
+                                <svg viewBox="0 0 1024 1024">
+                                <path d="M831.872 340.864 512 652.672 192.128 340.864a30.592 30.592 0 0 0-42.752 0 29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728 30.592 30.592 0 0 0-42.752 0z"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <div class="recent">
+                            <div>
+                                <input type="checkbox" name="user<?= "" ?>" value="" id="user">
+                                <label for="user">User</label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </form>
         </section>
